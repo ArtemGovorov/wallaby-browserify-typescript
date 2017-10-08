49973a92cf95a7e4da14b05ddfdd3866e2085bbe
Artem Govorov <artem.govorov@gmail.com>
2017-10-08 19:01:21 +1000

    Use Chrome runner

--- wallaby.js
+++ wallaby.js
@@ -11,6 +11,10 @@ module.exports = function () {
       {pattern: 'test/**/*.spec.ts', load: false}
     ],
 
+    env: {
+      kind: 'chrome'
+    },
+
     preprocessors: {
       '**/*.js': file => require('babel-core').transform(
         file.content,
